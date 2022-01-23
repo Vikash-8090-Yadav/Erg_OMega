03cf17dbd7afa1938c588a49f46482437245abc6
vikash yadav <yadav621gzp@gmail.com>
2022-01-23 22:59:00 +0530

    a new file added to the direcotry

---
+++ Leetcode/problem_solving/leetcode_easy_Q1331.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<int> arrayRankTransform(vector<int>& arr){
+	set<int> s;
+	map<int,int> m;
+	vector<int> v2,v3;
+	for(auto x : arr){
+		s.insert(x);
+	}
+	for(auto x :s){
+		v2.push_back(x);
+	}
+	for(int i = 0;i<v2.size();++i){
+		m[v2[i]] = i+1;
+	}
+	// for(auto x : m){
+	// 	cout<<x.first<<" "<<x.second<<" ";
+	// }
+	cout<<endl;
+	for(int  i = 0;i<arr.size();++i){
+		v3.push_back(m[arr[i]]);
+	}
+	return v3;
+
+}
+
+int main(){
+	vector<int> v ={100,100,100};
+	v = arrayRankTransform(v);
+	for(auto x : v){
+		cout<<x<<" ";
+	}
+}
