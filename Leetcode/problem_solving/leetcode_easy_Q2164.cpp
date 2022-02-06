8bbf9d95ac4597ca4a37c3de15fc46472cc4756d
vikash yadav <yadav621gzp@gmail.com>
2022-02-07 00:20:09 +0530

    un-necessary lines deleted

--- Leetcode/problem_solving/leetcode_easy_Q2164.cpp
+++ Leetcode/problem_solving/leetcode_easy_Q2164.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 
 vector<int> sortEvenOdd(vector<int>&nums){
-	vector<pair<int,int>> p;
+	
 	vector<int> v1;
 	vector<int> v4;
 	for(int i =0;i<nums.size();++i){		
@@ -16,7 +16,6 @@ vector<int> sortEvenOdd(vector<int>&nums){
 	int m1 =0;
 	sort(v4.begin(),v4.end());
 	for(int i =0;i<v4.size();++i){
-		// p.push_back({m,v4[i]});
 		m[m1] = v4[i];
 		m1+=2;
 	}
@@ -24,16 +23,12 @@ vector<int> sortEvenOdd(vector<int>&nums){
 	sort(v1.begin(),v1.end(),greater<int>());
 	for(int i =0;i<v1.size();++i){
 		m[j] = v1[i];
-
-		// p.push_back({j,v1[i]});
 		j+=2;
 	}
 	vector<int> final;
 	for(int i=0;i<nums.size();++i){
-		// cout<<p[i].second<<" ";
 		final.push_back(m[i]);
 	}
-	// cout<<p[1].second<<" ";
 
 	for(auto x : final){
 		cout<<x<<" ";
