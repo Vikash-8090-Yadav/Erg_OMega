d25d81a039eeb67ce73f8b893cb3c8af86e26a45
vikash yadav <yadav621gzp@gmail.com>
2022-05-25 03:48:17 +0530

    changed the code error

--- Solidity/nth_term.sol
+++ Solidity/nth_term.sol
@@ -3,34 +3,17 @@
 pragma solidity >=0.7.0 <0.9.0;
 
 
-contract nth{
-    // uint [] public arr;
-    // uint ans=0;
-    // function call() public  {
-    //     arr[0] =a;
-    //     arr[1] =b;
-    //     arr[2] =c;
-       
-    //     // ans=n;
-        
-    // }
-    // function get() view public returns(uint){
-    //     return arr[0]; 
-    // }
-        function nthTerm(
-        uint256 n,
-        uint256 a,
-        uint256 b,
-        uint256 c
-    ) public pure returns (uint256) {
-        uint256[100] memory arr;
-        uint256 i;
-        arr[1] = a;
-        arr[2] = b;
-        arr[3] = c;
-        for (i = 4; i <= n; i++) {
-            arr[i] = arr[i - 1] + arr[i - 2] + arr[i - 3];
+contract new_nth{
+
+    function nth(uint n,uint a,uint b,uint c) pure public returns(uint){
+        uint[100] memory arr;
+        arr[0] = a;
+        arr[1] =b;
+        arr[2] = c;
+        uint i;
+        for(i =3;i<n;++i){
+            arr[i] = arr[i-1]+arr[i - 2 ]+arr[i - 3];
         }
-        return arr[n];
+        return arr[n-1];
     }
 }
