d861332313e398405cb036c1a3c7102ffa449a07
Vikash KUmar Yadav <yadav621gzp@gmail.com>
2023-03-31 22:04:27 +0530

    aded the code for this

---
+++ Placement_Prep/C++/Linkedilist/SwapNodes.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class node{
+	int data;
+	node *next;
+	node(int data){
+		this->data=data;
+		next=NULL;
+	}
+};
+
+
+
+node *swapPair(node *head){
+	node * first = head;
+	node *second = head->next;
+}
+
+int  main(){
+
+}
