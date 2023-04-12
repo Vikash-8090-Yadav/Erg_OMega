78c94422d1aa8b6b9d789ddebf68a43a02cc3b30
Vikash KUmar Yadav <yadav621gzp@gmail.com>
2023-04-12 23:52:13 +0530

    added the leetcode problem to the directory

--- Placement_Prep/C++/BinaryTree/129.cpp
+++ Placement_Prep/C++/BinaryTree/129.cpp
@@ -16,16 +16,32 @@ class node{
 
 
 
-node *sumleaves(node *root){
-	
+int sumleaves(node *root,int sum){
+	if(!root) return 0;
 
-	
-	sumleaves(root->left);
-	sumleaves(root->right);
-
-
+	if(root->right ==NULL && root->left ==NULL){
+		sum = sum*10+root->data;
+		return sum;
+	}
+	sum  = sum*10+root->data;
+	return sumleaves(root->left , sum) + sumleaves(root->right,sum);
 
 }
 
 int main(){
+ 	 node *n1 = new node(1);
+         node *n2 = new node(2);
+         node *n3 = new node(3);
+         node *n4 = new node(4);
+         node *n5 = new node(5);
+         node *n6 = new node(6);
+         node *n7 = new node(7);
+         n1->left = n2;n1->right = n3;
+         n2->left = n4;n2->right = n5;
+  
+         n5->right = n6;
+
+	 cout<<sumleaves(n1,0);
+	 
+	
 }
